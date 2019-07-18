bc9305b68eab05a5705cbe7d8acf61fff83dae31
Jacek Sprzączkowski <yatzek@yatzek.pl>
2019-07-18 06:40:22 +0200

    syntax error replacement

--- js/script.js
+++ js/script.js
@@ -1,22 +1,27 @@
 'use strict';
 
-function titleClickHandler(event){
-  console.log('Link was clicked!');
+function titleClickHandler(event) {
+  event.preventDefault();
+  const clickedElement = this;
+  console.log('Link was clicked!', event);
 
-  /* remove class 'active' from all article links  */
+  /* [DONE] remove class 'active' from all article links  */
+
+/*[DONE] remove class 'active' from all article links  */
   const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-  /* add class 'active' to the clicked link */
+  /*[IN PROGRESS] add class 'active' to the clicked link */
+    clickedElement.classList.add('active');
+    console.log('clickedElement:', clickedElement);
+  /*[DONE] remove class 'active' from all articles */
+    const activeArticles = document.querySelectorAll('.post.active');
 
-  /* remove class 'active' from all articles */
-  const activeArticles = document.querySelectorAll('.post.active');
-
-for(let activeArticle of activeArticles){
-  activeArticle.classList.remove('active');
+    for(let activeArticle of activeArticles){
+      activeArticle.classList.remove('active');
 }
 
   /* get 'href' attribute from the clicked link */
